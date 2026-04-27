e55f1e0135419fa70cda13ccb5c7daf4ac1eeb2c
MarianCompany <samojlenko.wot@gmail.com>
2026-04-27 14:31:37 +0300

    feat: updated vite.config.js to split demo and progress

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: process.env.VITE_BASE_URL || '/',
+    build: {
+        rollupOptions: {
+            input: {
+                progress: resolve(__dirname, 'src/js/progress/index.js'),
+                demo: 'index.html'
+            },
+            output: {
+                entryFileNames: (chunkInfo) => {
+                    return chunkInfo.name === 'progress'
+                        ? 'progress/[name].js'
+                        : '[name].js'
+                },
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.names && assetInfo.names.some(name => name.includes('progress'))) {
+                        return 'progress/[name].[ext]'
+                    }
+                    return '[name].[ext]'
+                }
+            }
+        },
+        cssCodeSplit: true
+    }
 })
